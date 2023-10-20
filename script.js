8f7088f74b01d5923f0bf54486e45b4e497076ab
Asme <96826773+kuyasha21@users.noreply.github.com>
2023-10-20 21:43:22 +0600

    my portfolio

--- script.js
+++ script.js
@@ -20,13 +20,13 @@
 
 
 // menu bar changing while screen size change
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
+// };
 
 
 
